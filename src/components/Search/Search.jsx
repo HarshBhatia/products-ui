4d8dafa6fc47d25ca42bc9a51d24328426926967
Harsh <harshb.work@gmail.com>
2023-06-03 13:58:08 +0530

    Product UI init

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Search.css";
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState({ amazon: [], croma: [] });
+  const [isLoading, setLoading] = useState(false);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const res = await axios.get(
+      `http://localhost:5001/api/products/search?keyword=${query}`
+    );
+
+    setResults(res.data);
+    setLoading(false);
+  };
+
+  const openLink = (link) => {
+    window.open(link, "_blank");
+  };
+
+  return (
+    <div className="search-container">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isLoading ? (
+        <p className="loading">Loading... This may take a few seconds...</p>
+      ) : results.amazon.length ? (
+        <div className="results-container">
+          <div>
+            <h2>Amazon</h2>
+            {results.amazon.map((product, i) => (
+              <div
+                className="product-card"
+                key={i}
+                onClick={() => openLink(product.link)}
+              >
+                <img src={product.image} alt={product.description} />
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+              </div>
+            ))}
+          </div>
+
+          <div>
+            <h2>Croma</h2>
+            {results.croma.map((product, i) => (
+              <div
+                className="product-card"
+                key={i}
+                onClick={() => openLink(product.link)}
+              >
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>Search something to get started.</p>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
-import Tasks from './components/Tasks/Tasks'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Nav from './components/Nav/Nav';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Search from "./components/Search/Search";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/tasks",
-    element: <Tasks />,
+    path: "/",
+    element: <Search />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
